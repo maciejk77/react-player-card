cecaea536cb24a087e0b918478c690fbe0307eea
Maciej Kurek <maciejk77@gmail.com>
2017-09-24 13:21:57 +0100

    Express server set up, JSON and files in public folder

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var app = express();
+var data = require('./public/data/player-stats');
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+app.get('/', function (req, res) {
+  res.send(data);
+})
+
+app.get('/:playerName', function(req, res) {  
+  
+  // ES6 not working with Node, wrong logic? refactor, cleaner than loop below
+  // data.players.find(player => {
+  //   if(player.name.last === req.params.playerName) {
+  //     res.send(player);
+  //   }
+  // });
+
+  for(var i = 0; i < data.players.length; i++) {
+    if(data.players[i].player.name.last === req.params.playerName) {
+      res.send(data.players[i]);
+    }
+  } 
+});
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
